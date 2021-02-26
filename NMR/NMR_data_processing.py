bc36d6d89c89aee8692b24d37dbfcdd26211b5a3
Stefan <ga97mif@mytum.de>
2021-02-26 09:22:17 +0100

    new branch to test out the parsing

--- NMR/NMR_data_processing.py
+++ NMR/NMR_data_processing.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr  5 13:57:42 2019
-
+Adaption on Fri Feb 25 10:00:00 2021
 @author: snagel1
+@adapted: MichelleHaerder
 
-Script for emptyfile subtraction
+Script for NMR Phase Correction of Slice Samples
 --------------------------------
 
-This script reads plural NMR T2 measurement slice files as csv-files and one emptyfile.
-After subtraction of the emptyfile, the new corrected data for every single slice
-will be saved in a new csv-file with the same header as of the origin file. 
+This function reads plural NMR T2 measurement slice files as csv-files.
+It reads the foil measurements and subtracts its phase-corrected values from the slice samples.
+Also, it phase-corrects the h2o sample file
+The phase-corrected slice samples are saved as csv files with raw-data header.
+ 
 
 what to change?
 ---------------
@@ -19,7 +22,6 @@ importFolder/
 OutputFolder:    name of the dataset and/or folder
 
 """
-import argparse
 import numpy as np
 import os
 from FunctionsCollection import read_data, saving_in_csv, create_new_csv, PhaseCorrection, SubtractionNew, readPhase
@@ -27,85 +29,92 @@ from FunctionsCollection import read_data, saving_in_csv, create_new_csv, PhaseC
 # parser = argparse.ArgumentParser()
 # parser.add_argument("folderName", help="name of the folder containing experiment directories")
 # parser.add_argument("ExperimentKeyword",help="Mostly either 'NF' or 'DF'")
+def nmr_data_processing(folderName,verbose=True):
 
-#select importFolder to import files and exportFolder to export results
-importFolder = "Raw_data" 
-folderName = "3163_105_55_3DF" #adjust
-exportFolder = 'Processed_data'
-sampleFolder = ""
-foilFolder = ''
-h2oFolder = ''
-exportPath = os.path.join(exportFolder,folderName)
-allExperiments = os.listdir(os.path.join(importFolder,folderName))
-folderInitials = '3DF' #or 'DF', adjust
-#create export folder in process data with name exportPath if it doesnt exist already
-if not os.path.exists(exportPath):
-    os.mkdir(exportPath)
+    #Define import and export folders
+    importFolder = "Raw_data" 
+    folderName = "3163_105_55_3DF" #adjust
+    exportFolder = 'Processed_data'
+    sampleFolder = ""
+    foilFolder = ''
+    h2oFolder = ''
+    exportPath = os.path.join(exportFolder,folderName)
+    allExperiments = os.listdir(os.path.join(importFolder,folderName))
+    folderInitials = folderName.split("_")[-1]
+    #folderInitials = '3DF' #or 'DF', adjust
 
+    #create export folder in process data with name exportPath if it doesnt exist already
+    if not os.path.exists(exportPath):
+        os.mkdir(exportPath)
 
+    for experiment in allExperiments:
+        if folderInitials in experiment:
+            sampleFolder = experiment
+        if 'Folie'in experiment:
+            foilFolder = experiment
+        if 'H2O' in experiment:
+            h2oFolder = experiment
 
-for experiment in allExperiments:
-    if folderInitials in experiment:
-        sampleFolder = experiment
-    if 'Folie'in experiment:
-        foilFolder = experiment
-    if 'H2O' in experiment:
-        h2oFolder = experiment
+    #Take all .csv into list to analyse
+    allSlicesPath = os.path.join(importFolder,folderName,sampleFolder)
+    allSlices = os.listdir(allSlicesPath)
+    for theSlice in allSlices:
+        if not ".csv" in theSlice:
+            allSlices.remove(theSlice)
+    allSlices = sorted(allSlices)
 
-#Take all .csv into list to analyse
-allSlicesPath = os.path.join(importFolder,folderName,sampleFolder)
-allSlices = os.listdir(allSlicesPath)
-for theSlice in allSlices:
-    if not ".csv" in theSlice:
-        allSlices.remove(theSlice)
-allSlices = sorted(allSlices)
+    #read foil.csv
+    foilFile = ""
+    foilPath = os.path.join(importFolder,folderName,foilFolder)
+    allFoils = os.listdir(foilPath)
+    for theFoil in allFoils:
+        if ".csv" in theFoil:
+            foilFile = theFoil
 
-#read foil.csv
-foilFile = ""
-foilPath = os.path.join(importFolder,folderName,foilFolder)
-allFoils = os.listdir(foilPath)
-for theFoil in allFoils:
-    if ".csv" in theFoil:
-        foilFile = theFoil
+    T_foil, RV_foil, IV_foil, AV_foil = read_data(os.path.join(foilPath,foilFile))
+    Phase_foil = readPhase(os.path.join(foilPath,foilFile), 'foil')
+    APhaseCorrected_foil = PhaseCorrection(Phase_foil, RV_foil, IV_foil)
 
-T_foil, RV_foil, IV_foil, AV_foil = read_data(os.path.join(foilPath,foilFile))
-Phase_foil = readPhase(os.path.join(foilPath,foilFile), 'foil')
-APhaseCorrected_foil = PhaseCorrection(Phase_foil, RV_foil, IV_foil)
+    #read and correct h20.csv
+    h2oFile = ""
+    h2oPath = os.path.join(importFolder,folderName,h2oFolder)
+    allH2o = os.listdir(h2oPath)
+    for theH2o in allH2o:
+        if ".csv" in theH2o:
+            h2oFile = theH2o
 
-#read h20.csv
-h2oFile = ""
-h2oPath = os.path.join(importFolder,folderName,h2oFolder)
-allH2o = os.listdir(h2oPath)
-for theH2o in allH2o:
-    if ".csv" in theH2o:
-        h2oFile = theH2o
+    T_h2o, RV_h2o, IV_h2o, AV_h2o = read_data(os.path.join(h2oPath,h2oFile))
+    Phase_h2o = readPhase(os.path.join(h2oPath,h2oFile), 'h2o')
+    APhaseCorrected_h2o = PhaseCorrection(Phase_h2o, RV_h2o, IV_h2o)
+    data_h2o = create_new_csv(T_h2o, APhaseCorrected_h2o, os.path.join(h2oPath,h2oFile), os.path.join(exportFolder,folderName,'h2o.csv'))
+    saving_in_csv(data_h2o, os.path.join(exportFolder,folderName,'h2o.csv'))
+    if verbose:
+        print('H2O-Samples are corrected and saved')
+    #For loop over all slice samples
+    for theSlice in allSlices:
 
-T_h2o, RV_h2o, IV_h2o, AV_h2o = read_data(os.path.join(h2oPath,h2oFile))
-Phase_h2o = readPhase(os.path.join(h2oPath,h2oFile), 'h2o')
-APhaseCorrected_h2o = PhaseCorrection(Phase_h2o, RV_h2o, IV_h2o)
-data_h2o = create_new_csv(T_h2o, APhaseCorrected_h2o, os.path.join(h2oPath,h2oFile), os.path.join(exportFolder,folderName,'h2o.csv'))
-saving_in_csv(data_h2o, os.path.join(exportFolder,folderName,'h2o.csv'))
-#For loop over all slices
-for theSlice in allSlices:
+        ###Samplefile/ Emptyfile and folder definition
+        SampleName = theSlice 
+        SampleFile = os.path.join(importFolder,folderName,sampleFolder,SampleName)
+        T_sample, RV_sample, IV_sample, AV_sample = read_data(SampleFile)
+        Phase_sample = readPhase(SampleFile, SampleName) 
 
-    ###Samplefile/ Emptyfile and folder definition
-    SampleName = theSlice 
-    SampleFile = os.path.join(importFolder,folderName,sampleFolder,SampleName)
-    T_sample, RV_sample, IV_sample, AV_sample = read_data(SampleFile)
-    Phase_sample = readPhase(SampleFile, SampleName) 
+        #%%foil subtraction and/or phasecorrection
+        APhaseCorrected_sample = PhaseCorrection(Phase_sample, RV_sample, IV_sample)
+        AFinal = SubtractionNew(APhaseCorrected_sample, APhaseCorrected_foil)
 
-    #%%foil subtraction and/or phasecorrection
-    APhaseCorrected_sample = PhaseCorrection(Phase_sample, RV_sample, IV_sample)
-    AFinal = SubtractionNew(APhaseCorrected_sample, APhaseCorrected_foil)
+        #create exporting name
+        exportName = os.path.join(exportPath,SampleName)
+        if verbose:
+            print('Sample ', SampleName,': Foil subtraced and phase corrected! Exporting...')
+            
+        #%%saving
+        data_Samples = create_new_csv(T_sample, AFinal, SampleFile, exportName)
+        saving_in_csv(data_Samples, exportName)
+        if verbose:
+            print('Sample ', SampleName,':Exported to folder ',exportPath,'\n')
+            print('*****Processing next Sample*****\n')
+    if verbose:
+        print("Data processing completed")
 
-    #create exporting name
-    exportName = os.path.join(exportPath,SampleName)
-    print('Sample ', SampleName,': Foil subtraced and phase corrected! Exporting...')
-        
-    #%%saving
-    data_Samples = create_new_csv(T_sample, AFinal, SampleFile, exportName)
-    saving_in_csv(data_Samples, exportName)
-    print('Sample ', SampleName,':Exported to folder ',exportPath,'\n')
-    print('*****Processing next Sample*****\n')
-print("Done")
 
---
+++ NMR/testing.py
@@ -0,0 +1,3 @@
+theString = "lift_me_up"
+theString = theString.split("_")[-1]
+print(theString)
